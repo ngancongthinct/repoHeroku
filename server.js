533854241d4bf89e07b839ee417ce5f141301d75
ngancongthinct <ngancongthinct@gmail.com>
2022-03-16 21:17:17 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const jsonServer = require("json-server");
+const databaseFile = "database.json"; //Đặt trùng tên với file bên ngoài
+const server = jsonServer.create();
+const router = jsonServer.router(databaseFile);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3002;
+
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync(databaseFile);
+const db = low(adapter);
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// Handle POST, PUT and PATCH request
+server.use(jsonServer.bodyParser);
+
+// Setup others routes
+server.use(router);
+
+// Start server
+server.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
+
